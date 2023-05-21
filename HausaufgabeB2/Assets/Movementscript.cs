b395fb629e49751510f8acffe0b18f00dbdc7ccf
leawe20 <131157625+leawe20@users.noreply.github.com>
2023-05-21 19:08:45 +0200

    doge and rearangement

---
+++ HausaufgabeB2/Assets/Movementscript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fallingpieks start moving downwards
+
+        // doge start moving to th left 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if moving object colide with player -> game over
+    }
+}
--- HausaufgabeB2/Assets/Player.cs
+++ HausaufgabeB2/Assets/Player.cs
@@ -1,21 +1,19 @@
-//es sollen sich ja mindestens zwei objekte bewegen deshalb 
-//können wir die beiden piekser vlt so hoch schieben dass sie dann durchs bild fallen wenn der spieler dort ist
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingPiekser : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //player starts moving to right
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if player colides with anything -> game over
         
     }
 }
